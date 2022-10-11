921c2a1fe17973b6d668c3b8fe24d9c2b429b6f1
Elvis Salazar <e2.salazar.acuna.elvis@gmail.com>
2022-10-11 09:04:17 -0600

    Text updated

--- src/person/pages/InitialPage.jsx
+++ src/person/pages/InitialPage.jsx
@@ -11,7 +11,7 @@ export const InitialPage = () => {
           <h1>Elvis Salazar</h1>
           <p className="lead">Location: Chihuahua, México</p>
           <h2>About me</h2>
-          <p className="lead">I'm a ICT engineer specializing in software development; I have five years of experience doing software. My technical skills are focused on custom software and e-commerce; Technologies like JavasScript, Liquid, HTML, CSS, Node.JS, PHP, etc.</p>
+          <p className="lead">I'm a ICT engineer specializing in software development; I have five years of experience doing software. My technical skills are focused on custom software and e-commerce; Technologies like JavaScript, Liquid, HTML, CSS, Node.JS, PHP, etc.</p>
           <p className="lead">I like to work with multiple people; as a team lead, I managed teams from 5 to 10 developers, and I feel that communication is the primary key to completing projects. My personal goal is to earn a high level of performance that allows me to develop myself professionally and personally.</p>
           <h2>Social / Contact</h2>
           <a href="mailto:elvis@kanan.dev"><i className="fa-regular fa-envelope fa-2xl"></i></a>
